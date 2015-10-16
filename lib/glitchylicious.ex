3f7f637f8810f8ca7174ad2e31450858d95ba847
chris <chris@0010.io>
2015-10-16 13:53:04 +0200

    v 0.0.2. Add additional glitch mode.

--- lib/glitchylicious.ex
+++ lib/glitchylicious.ex
@@ -27,11 +27,22 @@ defmodule Glitchylicious do
       iter   - number of iterrations to perform.
       amount - corruption severety.
       seed   - seed value.
+      mode (optional) - Glitch mode. Currently :normal (default) or :reverse
   """
   @spec glitch(String.t, map) :: binary
-  def glitch(input, %{iter: iter, amount: amount, seed: seed}) do
+  def glitch(input, %{iter: iter, amount: amount, seed: seed, mode: mode}) do
     raw = File.read!(input)
-    do_glitch(raw, header_size(raw), iter, amount, seed)
+    case mode do
+      :normal -> 
+        do_glitch(raw, header_size(raw), iter, amount, seed)
+      :reverse ->
+        glitch_reverse(raw, header_size(raw), iter, amount, seed)
+      _ ->
+        do_glitch(raw, header_size(raw), iter, amount, seed)
+    end
+  end
+  def gltich(input, params = %{iter: iter, amount: amount, seed: seed}) do
+    glitch(input, Dict.put(params, :mode, :normal))
   end
 
   defp do_glitch(image, len, iter, amount, seed) do
@@ -53,6 +64,27 @@ defmodule Glitchylicious do
 
     replace(image, byte_index, Float.floor(amount * 256) |> trunc)
     |> do_glitch(len, i + 1, iter, amount, seed)
+  end
+
+  defp do_glitch_reverse(image, len, iter, amount, seed) do
+    do_glitch(image, len, iter, iter, amount / 100, seed / 100)
+  end
+  defp do_glitch_reverse(image, _len, 0, iter, _amount, _seed) do
+    image
+  end
+  defp do_glitch_reverse(image, len, i, iter, amount, seed) do
+    max_index = byte_size(image) - len - 4
+    px_min = round(max_index / iter * i)
+    px_max = round(max_index / iter * (i + 1))
+    delta = px_max - px_min
+    px_i = round(px_min + delta * seed)
+    
+    byte_index = if px_i > max_index,
+    do: len + max_index,
+    else: len + px_i
+
+    replace(image, byte_index, roud(amount * 256))
+    |> do_glitch(len, i - 1, iter, amount, seed)
   end
   
   defp header_size(raw), do: header_size(raw, 0)
